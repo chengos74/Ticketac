86beed4b18e3c9e2102d775b11c30e8943adb713
Mathieu <Mathieu@MacBook-Air-de-Mathieu-6.local>
2022-06-30 15:00:58 +0200

    userstrains

--- routes/users.js
+++ routes/users.js
@@ -4,18 +4,64 @@ var router = express.Router();
 var usersModel = require('../models/users');
 
 
-/* GET users listing. */
+// Get users listing
 router.get('/', function(req, res, next) {
     res.send('respond with a resource');
 });
 
-//Sign-in route
-router.post('/login', function(req,res) {
-	res.render ('index', )
 
 
-})
+// Sign-in route
+router.post('/sign-in', async function(req, res) {  
+  
+	var userSignedIn = await usersModel.findOne({
+	  email : req.body.email,
+	  password : req.body.password
+	});
+	// console.log(userSignedIn);
+  
+	if(userSignedIn) {
+	  req.session.user = {
+		name : userSignedIn.email,
+		id : userSignedIn._id
+	  } 
+	  // console.log(req.session.user);
 
+	  res.redirect('/');
+	} else {
+	  res.redirect('/login');
+	}
+  });
+
+
+
+// Sign-up route
+
+router.post('/sign-up', async function(req, res) {  
+  
+	var userExist = await usersModel.findOne({
+	  email : req.body.emailFromFront
+	})
+	
+	if(!userExist) {
+	  var newUser = new usersModel({
+		username : req.body.usernameFromFront,
+		email : req.body.emailFromFront,
+		password : req.body.passwordFromFront
+	  });
+	  var newUserSave = await newUser.save();
+  
+	  req.session.user = {
+		name : newUserSave.username,
+		id : newUserSave._id
+	  } 
+	  // console.log(req.session.user);
+
+	  res.redirect('/');
+	} else {
+	  res.redirect('/login');
+	}
+  });
 
 
 module.exports = router;
